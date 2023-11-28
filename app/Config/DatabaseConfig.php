fa807627ea43cd4b8904c9e3d1fb540298356fca
naufaril <96711362+naufaril@users.noreply.github.com>
2023-11-28 10:24:47 +0700

    Add files via upload

---
+++ app/Config/DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+
+    // SETTING KONFIGURASI DATABASE KALIAN
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "mod5";
+    public $port = 3306;
+
+}
